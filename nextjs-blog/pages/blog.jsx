e23808e4b139bf55f67821af8e341a9f88073e5c
akshaychavhan <akshay@rtledgers.com>
2023-10-16 18:54:21 +0530

    connection completed between frontend and backend

--- nextjs-blog/pages/blog.jsx
+++ nextjs-blog/pages/blog.jsx
@@ -1,43 +1,30 @@
-import React from "react";
-import styles from '../styles/Blog.module.css';
-
+import React, { useEffect, useState } from "react";
+import styles from "../styles/Blog.module.css";
+import Link from "next/link";
 
 export const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/blogs")
+      .then((item) => item.json())
+      .then((parsedItem) => setBlogs(parsedItem));
+  }, []);
+
   return (
     <>
       <div className={styles.blogs}>
-        <div className={styles.blogItem}>
-          <h3>How to learn Javascript in 2023 ?</h3>
-          <p>
-            dffbu we fiweufbe ewfbwiefbf wefbiufbewfeu fewfibfweufbwe
-            feiwfbeufbeuifbewjdffbu we fiweufbe ewfbwiefbf wefbiufbewfeu
-            fewfibfweufbwe feiwfbeufbeuifbewj
-          </p>
-        </div>
-        <div className={styles.blogItem}>
-          <h3>How to learn Javascript in 2023 ?</h3>
-          <p>
-            dffbu we fiweufbe ewfbwiefbf wefbiufbewfeu fewfibfweufbwe
-            feiwfbeufbeuifbewjdffbu we fiweufbe ewfbwiefbf wefbiufbewfeu
-            fewfibfweufbwe feiwfbeufbeuifbewj
-          </p>
-        </div>
-        <div className={styles.blogItem}>
-          <h3>How to learn Javascript in 2023 ?</h3>
-          <p>
-            dffbu we fiweufbe ewfbwiefbf wefbiufbewfeu fewfibfweufbwe
-            feiwfbeufbeuifbewjdffbu we fiweufbe ewfbwiefbf wefbiufbewfeu
-            fewfibfweufbwe feiwfbeufbeuifbewj
-          </p>
-        </div>
-        <div className={styles.blogItem}>
-          <h3>How to learn Javascript in 2023 ?</h3>
-          <p>
-            dffbu we fiweufbe ewfbwiefbf wefbiufbewfeu fewfibfweufbwe
-            feiwfbeufbeuifbewjdffbu we fiweufbe ewfbwiefbf wefbiufbewfeu
-            fewfibfweufbwe feiwfbeufbeuifbewj
-          </p>
-        </div>
+        <div  className={styles.blogItem}>
+        {blogs.map((blog , index) => {
+          return (
+            <div key={index}>
+              <Link href={`blogpost/${blog.slug}`}>
+                <h3>{blog.title}</h3>
+              </Link>
+              <p>{blog.content.substring(0,150)}</p>
+              </div>
+              );
+            })}
+            </div>
       </div>
     </>
   );
