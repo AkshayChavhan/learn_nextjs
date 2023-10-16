0394885cbee1264fb6f79a658217253253d2259c
akshaychavhan <akshay@rtledgers.com>
2023-10-16 23:16:26 +0530

    Fetching blogpost using /getpost endpoints

--- nextjs-blog/pages/blogpost/[slug].js
+++ nextjs-blog/pages/blogpost/[slug].js
@@ -1,24 +1,28 @@
 import { useRouter } from 'next/router'
 import styles from '../../styles/Blog.module.css';
+import { useEffect, useState } from 'react';
 
 
 const Slug = () => {
+
+  const [blog, setBlog] = useState([]);
   const router = useRouter()
-  const { slug } = router.query;
+  useEffect(() => {
+    const { slug } = router.query;
+    console.log("slug => ", slug);
+    if (!router.isReady) return;
+    // http://localhost:3000/blogpost/learn-css.json
+    fetch(`http://localhost:3000/api/getblog?slug=${slug}.json`)
+      .then((item) => item.json())
+      .then((data) => setBlog(data));
+  }, [router.isReady])
+
 
   return (
     <>
       <div className={styles.blogs}>
-        <h1>Title of the Post is {slug}</h1>
-        <p>HTTP requests allow your front-end application to interact successfully with a back-end server or database.
-
-          One of the five popular HTTP methods for making requests and interacting with your servers is the POST method, which you can use to send data to a server.
-
-          In this article, you will learn the various methods that you can use to send an HTTP POST request to your back-end server in JavaScript. We'll send GET requests to the free JSON Placeholder todos API for this guide.
-
-          There are two built-in JavaScript methods for making an HTTP POST request that don't require the installation of a library or the use of a CDN. These methods are the FetchAPI, based on JavaScript promises, and XMLHttpRequest, based on callbacks.
-
-          There are other methods, such as Axios and jQuery, that you will also learn how to use.</p>
+        <h1>{blog.title}</h1>
+        <p>{blog.content}</p>
       </div>
     </>
   )
