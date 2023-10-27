83513a4fb098676cc2eba700d66807a5504ba7cf
akshaychavhan <akshay@rtledgers.com>
2023-10-28 00:40:58 +0530

    Image Optimization in nextjs

--- nextjs_full_tut/blog/src/app/layout.js
+++ nextjs_full_tut/blog/src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import './styles.css'  //created new global css
 
 const inter = Inter({ subsets: ['latin'] })
 
--- nextjs_full_tut/blog/src/app/page.js
+++ nextjs_full_tut/blog/src/app/page.js
@@ -1,15 +1,36 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
-import styles from "./page.module.css";
+import Image from "next/image"
+import Profile from "../../public/vercel.svg";
 
 export default function Home() {
 
-  const router = useRouter();
   return (
     <main>
-      <h1>Welcome to Home Page</h1> {/*  using global css   */}
-      <h2 className={styles.header}>Welcome to Home Page</h2>  {/*  using module css   */}
+      <Image 
+      src={Profile}
+      alt="vercel logo"
+      height="200"
+      width="200"
+      />
+     {/* It will break
+      <img
+      src={Profile}
+      /> */}
+
+      <img
+      src = {Profile.src} // it will work
+      />
+
+{/* You will error for below 
+Error: Invalid src prop (https://unsplash.com/photos/a-police-car-parked-on-the-side-of-the-road-j0syEjr_l9o) on `next/image`,
+hostname "unsplash.com" is not configured under images in your `next.config.js` */}
+      <Image
+      src={"https://unsplash.com/photos/a-police-car-parked-on-the-side-of-the-road-j0syEjr_l9o"}
+      alt="vercel logo"
+      height="200"
+      width="200"
+      />
     </main>
   )
 }
