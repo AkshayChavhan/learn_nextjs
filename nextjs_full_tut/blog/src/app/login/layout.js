5ec0f157fc945013c48b16e29a43f6fbe4a47a49
akshaychavhan <akshay@rtledgers.com>
2023-10-27 11:25:09 +0530

    next07_Style added for hover

--- nextjs_full_tut/blog/src/app/login/layout.js
+++ nextjs_full_tut/blog/src/app/login/layout.js
@@ -8,13 +8,13 @@ export default function Layout({ children }) {
             <div className="login-nav">
                 <ul className="ul-style-none">
                     <li className="pad-right">
-                        <Link className="decoration-none" href="/login">Login Main</Link>
+                        <Link className="decoration-none nav-item" href="/login">Login Main</Link>
                     </li>
                     <li className="pad-right">
-                        <Link className="decoration-none" href="/login/student">Login Student</Link>
+                        <Link className="decoration-none nav-item" href="/login/student">Login Student</Link>
                     </li>
                     <li className="pad-right">
-                        <Link className="decoration-none" href="/login/teacher">Login Teacher</Link>
+                        <Link className="decoration-none nav-item" href="/login/teacher">Login Teacher</Link>
                     </li>
                 </ul>
             </div>
