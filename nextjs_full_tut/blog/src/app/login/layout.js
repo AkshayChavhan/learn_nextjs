92a142c952e5adba69dfe142e91070e7b8c4cad5
akshaychavhan <akshay@rtledgers.com>
2023-10-27 11:54:16 +0530

    next08_conditional layout navbar added for teachers route

--- nextjs_full_tut/blog/src/app/login/layout.js
+++ nextjs_full_tut/blog/src/app/login/layout.js
@@ -1,23 +1,38 @@
+'use client'
 import Link from "next/link";
 import "./style.css";
+import { usePathname } from "next/navigation";
 
 
 export default function Layout({ children }) {
+
+    const pathName = usePathname();
+    console.log(pathName);
     return (
         <div>
-            <div className="login-nav">
-                <ul className="ul-style-none">
-                    <li className="pad-right">
-                        <Link className="decoration-none" href="/login">Login Main</Link>
-                    </li>
-                    <li className="pad-right">
-                        <Link className="decoration-none" href="/login/student">Login Student</Link>
-                    </li>
-                    <li className="pad-right">
-                        <Link className="decoration-none" href="/login/teacher">Login Teacher</Link>
-                    </li>
-                </ul>
-            </div>
+            {
+                (pathName !== '/login/teacher') ?
+                    <div className="login-nav">
+                        <ul className="ul-style-none">
+                            <li className="pad-right">
+                                <Link className="decoration-none" href="/login">Login Main</Link>
+                            </li>
+                            <li className="pad-right">
+                                <Link className="decoration-none" href="/login/student">Login Student</Link>
+                            </li>
+                            <li className="pad-right">
+                                <Link className="decoration-none" href="/login/teacher">Login Teacher</Link>
+                            </li>
+                        </ul>
+                    </div> :
+                    <div className="login-nav">
+                        <ul className="ul-style-none">
+                            <li className="pad-right">
+                                <Link className="decoration-none" href="/login">Login Main</Link>
+                            </li>
+                        </ul>
+                    </div>
+            }
             {children}
         </div>
     )
