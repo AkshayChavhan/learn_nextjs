0c43c1b0dc3078f8d10d96cf370a690f9d28d36c
akshaychavhan <akshay@rtledgers.com>
2023-10-27 12:24:14 +0530

    next09_dynamic routing added for student list

---
+++ nextjs_full_tut/blog/src/app/studentlist/[student]/page.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function StudentDetail(props){
+
+    // console.log(props);
+
+    return(
+        <h1>Welcome to student details of {props.params.student}</h1>
+    )
+} 
