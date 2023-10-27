44c7da5099b11658d08a1557e28d4a4e133cd8aa
akshaychavhan <akshay@rtledgers.com>
2023-10-27 09:02:25 +0530

    Basic file based routing 2 example completed

---
+++ nextjs_full_tut/blog/src/app/about/page.js
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div>
+      <h1>Hello About pages</h1>
+    </div>
+  )
+}
+
+export default About;
--- nextjs_full_tut/blog/src/app/page.js
+++ nextjs_full_tut/blog/src/app/page.js
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Hi there</h1>
+      <h1>Welcome to Home Page</h1>
     </main>
   )
 }
