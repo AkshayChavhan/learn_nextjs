30fc6d0e05767a09d970481b010ca1cf92dcc659
akshaychavhan <akshay@rtledgers.com>
2023-10-30 15:31:07 +0530

    Post api created and connected to frontend

--- nextjs_full_tut/blog/src/app/api/users/route.js
+++ nextjs_full_tut/blog/src/app/api/users/route.js
@@ -6,3 +6,13 @@ export async function GET(request) {
     const data = user;
     return NextResponse.json( data, { status: 200 });
 }
+
+export async function POST( request ){
+    let parseData = await request.json();
+    const { name , age , id} = parseData ;
+    if( !name || !age || !id){
+        return NextResponse.json({message : "Request fields not provided"} ,{status : 400})
+    }
+    // We are not creating user just added message for now
+    return NextResponse.json({message : "New User Created"} ,{status : 201})
+}
